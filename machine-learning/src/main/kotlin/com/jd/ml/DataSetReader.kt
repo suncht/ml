5e2c9e75ed47cd2bc0459cb8588c2d8a40760eea
sunchangtan <sunchangtan@jd.com>
2018-08-18 18:22:43 +0800

    提交

--- machine-learning/src/main/kotlin/com/jd/ml/DataSetReader.kt
+++ machine-learning/src/main/kotlin/com/jd/ml/DataSetReader.kt
@@ -14,26 +14,59 @@ import java.io.FileReader
 data class DataSet(val features: Matrix, val label: Vector)
 
 object DataSetReader {
-    fun loadFromCsv(fileName: String): DataSet? {
+    fun loadFromCsv(fileName: String, onehot: Boolean = false): DataSet {
         val reader = FileReader(fileName)
-        val parser = CSVFormat.DEFAULT.withHeader("area", "floor", "orientation", "price").parse(reader)
-        val rowNumber = parser.recordNumber.toInt()
-        val features = Matrix(rowNumber, 3)
-        val label = Vector(rowNumber)
-        for(record in parser) {
-            println(record["area"])
-        }
+        val parser = CSVFormat.DEFAULT.withHeader("area", "floor", "orientation", "price").withTrim()
+                .withSkipHeaderRecord().parse(reader)
+        val records = parser.records
         reader.close()
-        return null
+
+        val rowNumber = records.size
+        var features = Matrix(rowNumber, 3)
+        val label = Vector(rowNumber)
+
+        val orientationVector = mutableListOf<String>()
+
+        records.forEachIndexed { index, csvRecord ->
+            features.setData(index, 0, csvRecord["area"].toDouble())
+            features.setData(index, 1, csvRecord["floor"].toDouble())
+
+            orientationVector += csvRecord["orientation"]
+
+            label.setData(index, csvRecord["price"].toDouble())
+        }
+
+        val orientationMatrix = oneHot(orientationVector)
+        features = features.replaceColumn(2, orientationMatrix)
+
+
+        return DataSet(features, label)
     }
 
+    private fun oneHot(data: List<String>): Matrix {
+        var unqueData = mutableSetOf<String>()
+        unqueData.addAll(data)
+        val dataIndex = mutableListOf<Int>()
+        for (item in data) {
+            dataIndex += unqueData.indexOf(item)
+        }
 
+        val dim1 = data.size
+        val dim2 = unqueData.size
+        val matrix = Matrix(dim1, dim2)
+
+        dataIndex.forEachIndexed { i, j ->
+            matrix.setData(i, j, 1.0)
+        }
+
+        return matrix
+    }
 
 }
 
 
 fun main(args: Array<String>) {
-    DataSetReader.loadFromCsv("E:\\开发\\JavaWorkspace\\ml\\machine-learning\\data\\price.csv")
-
+    val ds = DataSetReader.loadFromCsv("E:\\开发\\JavaWorkspace\\ml\\machine-learning\\data\\price.csv")
+    println(ds.features)
 }
 
